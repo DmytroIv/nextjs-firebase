4f5645dee5bc4b9f0ddec540e892d0f53a15949e
DmytroIv <dmytropopovkh@gmail.com>
2022-08-09 12:13:08 +0300

    Next.js init

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js + Firebase</title>
+        <meta name="description" content="Next.js + Firebase" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
